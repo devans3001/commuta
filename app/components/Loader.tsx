c3437dbd5ddbfd0ace258111775ca8e4ae86586b
Evans Diegbe <devans3001@gmail.com>
2025-12-22 18:31:56 +0100

    unique loader

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -44,7 +44,7 @@ function Loading() {
   return (
     <>
     {/* <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"> */}
-      <RandomSpinner size={30} />
+      <GridLoader size={30} />
     </>
   );
 }
